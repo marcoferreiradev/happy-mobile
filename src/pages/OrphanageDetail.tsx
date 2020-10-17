479e9e7c785805814fdeb57ab84c9febef778a98
Marco Willian Bezerra Ferreira <marco.ferreira@enext.com.br>
2020-10-17 15:02:12 -0300

    feat: initial commit and initial page  OrphanageMap

---
+++ src/pages/OrphanageDetail.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function OrphanageDetail() {
+  return (
+    <View>
+      <Text>
+        Text Orphanage Detail
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
